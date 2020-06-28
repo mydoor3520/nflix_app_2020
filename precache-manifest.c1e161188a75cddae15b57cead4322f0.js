5956d75d983abc338a9b84ed96bed4d55b4e77ae
mydoor3520 <mydoor3520@naver.com>
2020-06-28 23:32:20 +0900

    Updates

---
+++ precache-manifest.c1e161188a75cddae15b57cead4322f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df72b990684cf7ca64d83e6955c78958",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4892d39426ad93921b69",
+    "url": "/movie_app_2020/static/css/main.5e128132.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4892d39426ad93921b69",
+    "url": "/movie_app_2020/static/js/main.134f1c34.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
